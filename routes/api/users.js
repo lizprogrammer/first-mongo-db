1e713983a2cc34b69eff69fb36c7442011bd113a
Elizabeth Rosso <lizprogrammer@yahoo.com>
2018-10-28 19:54:54 -0400

    adding avatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 
 // Load User model
 const User = require('../../models/User');
@@ -7,7 +8,7 @@ const User = require('../../models/User');
 // @route   GET api/users/test
 // @desc    Tests users route
 // @access  Public
-router.get('/test', (req, res)  => res.json({msg: "Users works"}));
+router.get('/test', (req, res)  => res.json({msg: 'Users works'}));
 
 // @route   GET api/users/register
 // @desc    Register user
@@ -15,6 +16,23 @@ router.get('/test', (req, res)  => res.json({msg: "Users works"}));
 
 router.post('/register', (req, res) => {
   User.findOne({ email: req.body.email })
+    .then(user => {
+      if(user) {
+        return res.status(400).json({email: 'Email already exists'});
+      } else {
+        const avatar = gravatar.url(req.body.email,{
+          s: '200', // Size
+          r: 'pg',  // Rating
+          d: 'mm'   // Default
+        });
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          avatar,
+          password: req.body.password
+        });
+      }
+    })
 });
 
 module.exports = router;
