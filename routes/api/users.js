7ce45f0257c55aa0ad72ae7529ee4bb4df75fb09
Elizabeth Rosso <lizprogrammer@yahoo.com>
2018-11-15 18:44:50 -0500

    code

--- routes/api/users.js
+++ routes/api/users.js
@@ -40,6 +40,8 @@ router.get('/:name', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  const{ name, password, avatar } = req.body;
+
   const newUser = new User({
     name,
     password,
@@ -47,6 +49,23 @@ router.post('/', (req, res) => {
   })
   
   newUser.save()
+    .then(user => res.status(201).json(user))
+    .catch(err => {
+      res.status(500).json({message: err})
+    });
+})
+
+router.delete('/:name', (req, res) => {
+  // TODO: protected route ensure the user is the one deleting
+  const {name, password, avatar} = req.params;
+  User.findOne({ name : name })
+    .then(user => {
+      if(!user){
+        return res.status(404).json({message: `User: ${name} not found`})
+      }
+      res.json(user);
+    })
+    .catch(err => res.status(500).json({message: err}));
 })
 
 // router.post('/register', (req, res) => {
